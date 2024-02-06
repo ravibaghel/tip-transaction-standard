ae336345b82d8d1d6751c03790fbc66a4f48a9ae
Ravi Baghel <ravibaghel@outlook.com>
2024-02-05 21:52:10 -0500

    Added Core project

---
+++ Baghel.TIP.Core/Common/TransactionHeader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baghel.TIP.Core.Common
+{
+    public class TransactionHeader
+    {
+        
+    }
+}
